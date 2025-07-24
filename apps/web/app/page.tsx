2eafb03912f10fcf6b1f9c203f2d88ab2e29fcd8
Zack <lorenx@163.com>
2025-07-24 16:09:06 +0800

    chore: update react-hook-form to version 7.61.0 and upgrade vite to version 7.0.6 across the project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,10 +2,14 @@
 
 import { Button } from "@raypx/ui/components/button"
 import { toast } from "@raypx/ui/components/sonner"
+import { useTheme } from "next-themes"
 
 export default function Home() {
+  const { theme, setTheme } = useTheme()
+
   const onClick = () => {
     toast.success("Hello World")
+    setTheme(theme === "dark" ? "light" : "dark")
   }
   return (
     <div>
