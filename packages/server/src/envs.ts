f01920067812324bc4c788e93ed22accca11a88a
Zack <lorenx@163.com>
2025-07-30 23:30:08 +0800

    feat: add database dependency and integrate it into server environment and routes

--- packages/server/src/envs.ts
+++ packages/server/src/envs.ts
@@ -1,7 +1,9 @@
+import { envs as db } from "@raypx/db/envs"
 import { createEnv } from "@raypx/shared"
 
 export const envs = () =>
   createEnv({
+    extends: [db()],
     client: {},
     shared: {},
     server: {},
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,18 +1,39 @@
+import { type Database, db } from "@raypx/db"
 import { Hono } from "hono"
-
-const app = new Hono()
-
-app.get("/", (c) => {
-  return c.text("Hello World")
-})
+import { compress } from "hono/compress"
+import { contextStorage } from "hono/context-storage"
+import { cors } from "hono/cors"
+import { logger } from "hono/logger"
+import { prettyJSON } from "hono/pretty-json"
+import { requestId } from "hono/request-id"
 
 interface ServerOptions {
   prefix: string
 }
 
-export const createApp = (options: ServerOptions) => {
-  const app = new Hono().basePath(options.prefix)
+type Variables = {
+  db: Database
+}
 
+export const createApp = (options: ServerOptions) => {
+  const app = new Hono<{ Variables: Variables }>().basePath(options.prefix)
+
+  // Middlewares
+  app
+    .use(logger())
+    .use(cors())
+    .use(compress())
+    .use(requestId())
+    .use(contextStorage())
+    .use(prettyJSON())
+
+  // Database
+  app.use("*", async (c, next) => {
+    c.set("db", db)
+    await next()
+  })
+
+  // Routes
   app.get("/", (c) => {
     return c.text("Hello World")
   })
